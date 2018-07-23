503812aa31d014bcf9d1704cc5ab39d03aa2e91d
Alex Noble <AWGR-Alex@comcast.net>
2018-07-22 23:44:41 -0700

    make timer count down

--- EventList.js
+++ EventList.js
@@ -1,7 +1,16 @@
 import React, {Component} from 'react';
-import { FlatList, Text } from 'react-native';
+import { FlatList, Text, StyleSheet } from 'react-native';
 
 import EventCard from './EventCard';
+
+const styles = StyleSheet.create({
+    list: {
+        flex: 1,
+        paddingTop: 20,
+        backgroundColor: '#F3F3F3'
+    },
+});
+
 
 class EventList extends Component {
     state = {
@@ -9,6 +18,15 @@ class EventList extends Component {
     }
 
     componentDidMount(){
+        setInterval(() =>{
+            this.setState({
+                events: this.state.events.map(evt => ({
+                    ...evt,
+                    timer: Date.now(),
+                })),
+            });
+        }, 1000);
+
         const events = require('./db.json').events.map(e => ({
             ...e,
             date: new Date(e.date),
--- Eventcard.js
+++ Eventcard.js
@@ -21,6 +21,45 @@ const styles = StyleSheet.create({
         marginTop: 5,
         marginBottom: 5,
     },
+    cardHeader: {
+        flex: 1,
+        flexDirection: 'row',
+    },
+    date: {
+        fontWeight: '200',
+        fontSize: 15,
+        color: '#bdbdbd',
+        width: '30%',
+        textAlign: 'right',
+    },
+    title: {
+        fontSize: 15,
+        fontWeight: '300',
+        marginLeft: 7,
+        textAlign: 'left',
+    },
+    counterContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingLeft: '5%',
+        paddingRight: '5%',
+    },
+    counter: {
+        width: '25%',
+        flex:1,
+    },
+    counterText:{
+        fontSize:40,
+        textAlign: 'center',
+    },
+    counterLabel: {
+        fontSize:13,
+        fontWeight: '100',
+        color: '#a3a3a3',
+        textAlign: 'center',
+        paddingTop: 0,
+    },
 });
 
 export default function EventCard ({ event}) {
