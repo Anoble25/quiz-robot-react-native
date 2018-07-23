7542b09f2ab7970660f90420459047e7a23efe8a
Alex Noble <AWGR-Alex@comcast.net>
2018-07-22 22:49:59 -0700

    add flat list

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import EventList from './EventList';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={{color: '#566573', fontSize: 40}}>Quiz Robot - React-Native app</Text>
-      </View>
+      <EventList/>
     );
   }
 }
