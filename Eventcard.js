6327b5177b92ef2e14903a214580cb29eab78ad4
Alex Noble <AWGR-Alex@comcast.net>
2018-07-22 23:03:24 -0700

    add api.js

---
+++ Eventcard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {
+    Text,
+    View,
+    Stylesheet,
+} from 'react-native';
+import PropTypes from 'prop-types';
+import {
+    formatdate,
+    getCountdownParts,
+} from './api';
