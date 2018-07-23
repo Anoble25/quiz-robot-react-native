43343e4f5369f0a50cec3f182d59dc0818f6d106
Alex Noble <AWGR-Alex@comcast.net>
2018-07-22 21:54:08 -0700

    increase font size

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Quiz Robot - React-Native app</Text>
+        <Text style={{color: '#566573', fontSize: 40}}>Quiz Robot - React-Native app</Text>
       </View>
     );
   }
